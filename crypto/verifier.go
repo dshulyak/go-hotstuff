0218fd2a1383ef052e08f88750a6732ac4b3fdb2
dmitry <yashulyak@gmail.com>
2020-01-19 16:41:08 +0200

    Add verifier based on ed25519
    Signed-off-by: dmitry <yashulyak@gmail.com>

---
+++ crypto/verifier.go
@@ -0,0 +1,47 @@
+package crypto
+
+import (
+	"crypto/ed25519"
+
+	"github.com/dshulyak/go-hotstuff/types"
+)
+
+/*
+Placeholder for BLS signatures.
+*/
+
+func NewEd25519Verifier(threshold int, pubkeys map[uint64]ed25519.PublicKey) *Ed25519Verifier {
+	return &Ed25519Verifier{
+		threshold: threshold,
+		pubkeys:   pubkeys,
+	}
+}
+
+type Ed25519Verifier struct {
+	threshold int
+	pubkeys   map[uint64]ed25519.PublicKey
+}
+
+func (v *Ed25519Verifier) VerifySingle(idx uint64, msg, sig []byte) bool {
+	pub, exist := v.pubkeys[idx]
+	if !exist {
+		return false
+	}
+	return ed25519.Verify(pub, msg, sig)
+}
+
+func (v *Ed25519Verifier) VerifyCert(cert *types.Certificate) bool {
+	count := 0
+	for i, idx := range cert.Voters {
+		valid := v.VerifySingle(idx, cert.Block, cert.Sigs[i])
+		if !valid {
+			return false
+		}
+	}
+	return count >= v.threshold
+}
+
+func (v *Ed25519Verifier) Merge(cert *types.Certificate, vote *types.Vote) {
+	cert.Voters = append(cert.Voters, vote.Voter)
+	cert.Sigs = append(cert.Sigs, vote.Sig)
+}
